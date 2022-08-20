ec3c73d944e5cb347990e66621c84275085d6a0c
Krishna Ravishankar <25255817+krsh732@users.noreply.github.com>
2022-08-20 16:51:10 -0400

    More drafting with @birdstakes
    Co-authored-by: birdstakes <birdstakes@users.noreply.github.com>

--- qvm_decompiler/__main__.py
+++ qvm_decompiler/__main__.py
@@ -66,12 +66,10 @@ class Opcode(IntEnum):
     CVFI = 59
 
 class ASTNode(ABC):
-    children: list #[ASTNode]?
-
     @abstractmethod
-    def __str__(self):
+    def __items__(self):
         pass
-    
+
     @abstractmethod
     def __repr__(self):
         pass
@@ -83,8 +81,8 @@ class Constant(Expr):
     def __init__(self, value):
         self.value = value
 
-    def __str__(self):
-        return f"{self.value:#x}"
+    def __items__(self):
+        yield from ()
     
     def __repr__(self):
         return f"Constant(value={self.value:#x})"
@@ -94,22 +92,15 @@ class UnaryOp(Expr):
     OP: str
 
     def __init__(self, target):
-        self.children = [target]
+        self.target = target
 
-    @property
-    def target(self):
-        return self.children[0]
-
-    def __str__(self):
-        return f"{self.OP}{stringify_expr(self.target)}"
+    def __items__(self):
+        yield self.target
     
     def __repr__(self):
         return f"{self.__class__.__name__}(target={self.target!r})"
 
-class NegI(UnaryOp):
-    OP = '-'
-
-class NegF(UnaryOp):
+class Neg(UnaryOp):
     OP = '-'
 
 class BCom(UnaryOp):
@@ -119,18 +110,12 @@ class BinOp(Expr):
     OP: str
     
     def __init__(self, lhs, rhs):
-        self.children = [lhs, rhs]
+        self.lhs = lhs
+        self.rhs = rhs
 
-    @property
-    def lhs(self):
-        return self.children[0]
-    
-    @property
-    def rhs(self):
-        return self.children[1]
-
-    def __str__(self):
-        return f"{stringify_expr(self.lhs)} {self.OP} {stringify_expr(self.rhs)}"
+    def __items__(self):
+        yield self.lhs
+        yield self.rhs
     
     def __repr__(self):
         return f"{self.__class__.__name__}(lhs={self.lhs!r}, rhs={self.rhs!r})"
@@ -141,22 +126,13 @@ class Add(BinOp):
 class Sub(BinOp):
     OP = '-'
 
-class DivI(BinOp):
+class Div(BinOp):
     OP = '/'
 
-class DivU(BinOp):
-    OP = '/'
-
-class ModI(BinOp):
+class Mod(BinOp):
     OP = '%'
 
-class ModU(BinOp):
-    OP = '%'
-
-class MulI(BinOp):
-    OP = '*'
-
-class MulU(BinOp):
+class Mul(BinOp):
     OP = '*'
 
 class And(BinOp):
@@ -171,23 +147,8 @@ class Xor(BinOp):
 class Lsh(BinOp):
     OP = '<<'
 
-class RshI(BinOp):
+class Rsh(BinOp):
     OP = '>>'
-
-class RshU(BinOp):
-    OP = '>>'
-
-class AddF(BinOp):
-    OP = '+'
-
-class SubF(BinOp):
-    OP = '-'
-
-class DivF(BinOp):
-    OP = '/'
-
-class MulF(BinOp):
-    OP = '*'
 
 class Assign(BinOp):
     OP = '='
@@ -201,237 +162,162 @@ class Eq(Comparison):
 class Ne(Comparison):
     OP = "!="
 
-class LtI(Comparison):
+class Lt(Comparison):
     OP = "<"
 
-class LeI(Comparison):
+class Le(Comparison):
     OP = "<="
 
-class GtI(Comparison):
+class Gt(Comparison):
     OP = ">"
 
-class GeI(Comparison):
+class Ge(Comparison):
     OP = ">="
-
-class LtU(Comparison):
-    OP = "<"
-
-class LeU(Comparison):
-    OP = "<="
-
-class GtU(Comparison):
-    OP = ">"
-
-class GeU(Comparison):
-    OP = ">="
-
-class EqF(Comparison):
-    OP = "=="
-
-class NeF(Comparison):
-    OP = "!="
-
-class LtF(Comparison):
-    OP = "<"
-
-class LeF(Comparison):
-    OP = "<="
-
-class GtF(Comparison):
-    OP = ">"
-
-class GeF(Comparison):
-    OP = ">="        
 
 class Deref(Expr):
     def __init__(self, size, target):
         self.size = size
-        self.children = [target]
+        self.target = target
     
-    @property
-    def target(self):
-        return self.children[0]
+    def __items__(self):
+        yield self.target
 
-    def __str__(self):
-        return f"*{stringify_expr(self.target)}"
-    
     def __repr__(self):
         return f"Deref(size={self.size!r}, target={self.target!r})"
 
 class Ref(Expr):
     def __init__(self, target):
-        self.children = [target]
+        self.target = target
 
-    @property
-    def target(self):
-        return self.children[0]
-
-    def __str__(self):
-        return f"&{stringify_expr(self.target)}"
+    def __items__(self):
+        yield self.target
 
     def __repr__(self):
         return f"Ref(target={self.target!r})"
 
 class FunCall(Expr):
     def __init__(self, target, args):
-        self.children = [target] + args
+        self.target = target
+        self.args = args
     
-    @property
-    def target(self):
-        return self.children[0]
-
-    @property
-    def args(self):
-        return self.children[1:]
-
-    def __str__(self):
-        return f"{stringify_expr(self.target)}({', '.join(str(arg) for arg in self.args)})"
+    def __items__(self):
+        yield self.target
+        yield from self.args
 
     def __repr__(self):
         return f"FunCall(target={self.target!r}, args={self.args!r})"
 
-class BlockCopy(Assign): # TODO: See below.
-    """This is sketch? Maybe BlockCopy shouldn't exist and Assign should just take size cause it has multiple stores anyway...
-    Counter point:
-    typedef struct {                0x00000000  vmMain:             ENTER       0x10                            
-        int a;                      0x00000001                      LOCAL       var_8                           
-    } s;                            0x00000002                      LOCAL       var_c                           
-    void test( void ) {             0x00000003                      BLOCK_COPY  0x4                             
-        s x;                        0x00000004                      PUSH                                        
-        s y;                        0x00000005                      LEAVE       0x10                            
-        x = y;
-    }
-    Counter counter point: Maybe assign can have a source attribute indicating it originated from BLOCK_COPY or something."""
-    def __init__(self, size, lhs, rhs):
-        super().__init__(lhs, rhs)
-        self.size = size
-
-    def __str__(self):
-        return f"{super().__str__()} /* size: {self.size} */"
-    
-    def __repr__(self):
-        return f"BlockCopy(size={self.size}, lhs={self.lhs}, rhs={self.rhs})"
-
-class StackVar(ASTNode):  # TODO: should this derive from an expr or something?
+class StackVar(Expr):
     def __init__(self, offset):
         self.offset = offset
     
-    def __str__(self):
-        return f"local_{self.offset:x}"
-    
+    def __items__(self):
+        yield from ()
+
     def __repr__(self):
         return f"StackVar(offset={self.offset:#x})"
 
-def stringify_expr(expr): # TODO: Maybe a pretty printer visitor pass or something should be the one to handle these kinds of things...
-    if isinstance(expr, Constant) or isinstance(expr, StackVar):
-        return str(expr)
-    return f"({expr})"
+class Cast(Expr):
+    def __init__(self, expr, type):
+        self.expr = expr
+        self.type = type
+    
+    def __items__(self):
+        yield self.expr
+    
+    def __repr__(self):
+        return f"Cast(expr={self.expr!r}, type={self.type!r})"
 
 class Stmt(ASTNode):
     pass
 
 class ExprStmt(Stmt):
     def __init__(self, expr):
-        self.children[0] = expr
-    
-    @property
-    def expr(self):
-        return self.children[0]
-    
-    def __str__(self):
-        return f"{self.expr};"
+        self.expr = expr
+
+    def __init__(self):
+        yield self.expr
     
     def __repr__(self):
         return f"ExprStmt(expr={self.expr!r})"
 
 class Block(Stmt):
     def __init__(self, stmts):
-        self.children = stmts
+        self.stmts = stmts
     
-    def __str__(self): # TODO: implicit vs explicit thing
-        lf_tab = "\n\t"
-        return f"{{\n\t{lf_tab.join(str(child) for child in self.children)}}}"
+    def __items__(self):
+        yield from self.stmts
     
     def __repr__(self):
         return f"Block(stmts={self.children!r})"
 
 class Goto(Stmt):
     def __init__(self, target):
-        self.children = [target]
+        self.target = target
     
-    @property
-    def target(self):
-        return self.children[0]
-    
-    def __str__(self):
-        return f"goto {self.target};"
+    def __items__(self):
+        yield self.target
     
     def __repr__(self):
         return f"Goto(target={self.target!r})"
 
 class Return(Stmt):
     def __init__(self, value):
-        self.children = [value]
+        self.value = value
     
-    @property
-    def value(self):
-        return self.children[0]
-    
-    def __str__(self):
-        if self.value is None:
-            return "return;"
-        return f"return {self.value};"
+    def __items__(self):
+        if self.value:
+            yield self.value
     
     def __repr__(self):
         return f"Return(value={self.value!r})"
     
-
 def _read_int_32(f):
     return int.from_bytes(f.read(4), "little", signed=True)
 
 comparison_map = {
     Opcode.EQ: Eq,
     Opcode.NE: Ne,
-    Opcode.LTI: LtI,
-    Opcode.LEI: LeI,
-    Opcode.GTI: GtI,
-    Opcode.GEI: GeI,
-    Opcode.LTU: LtU,
-    Opcode.LEU: LeU,
-    Opcode.GTU: GtU,
-    Opcode.GEU: GeU,
-    Opcode.EQF: EqF,
-    Opcode.NEF: NeF,
-    Opcode.LTF: LtF,
-    Opcode.LEF: LeF,
-    Opcode.GTF: GtF,
-    Opcode.GEF: GeF
+    Opcode.LTI: Lt,
+    Opcode.LEI: Le,
+    Opcode.GTI: Gt,
+    Opcode.GEI: Ge,
+    Opcode.LTU: Lt,
+    Opcode.LEU: Le,
+    Opcode.GTU: Gt,
+    Opcode.GEU: Ge,
+    Opcode.EQF: Eq,
+    Opcode.NEF: Ne,
+    Opcode.LTF: Lt,
+    Opcode.LEF: Le,
+    Opcode.GTF: Gt,
+    Opcode.GEF: Ge
 }
 
 bin_op_map = {
     Opcode.ADD: Add,
     Opcode.SUB: Sub,
-    Opcode.DIVI: DivI,
-    Opcode.DIVU: DivU,
-    Opcode.MODI: ModI,
-    Opcode.MODU: ModU,
-    Opcode.MULI: MulI,
-    Opcode.MULU: MulU,
+    Opcode.DIVI: Div,
+    Opcode.DIVU: Div,
+    Opcode.MODI: Mod,
+    Opcode.MODU: Mod,
+    Opcode.MULI: Mul,
+    Opcode.MULU: Mul,
     Opcode.BAND: And,
     Opcode.BOR: Or,
     Opcode.BXOR: Xor,
     Opcode.LSH: Lsh,
-    Opcode.RSHI: RshI,
-    Opcode.RSHU: RshU,
-    Opcode.ADDF: AddF,
-    Opcode.SUBF: SubF,
-    Opcode.DIVF: DivF,
-    Opcode.MULF: MulF
+    Opcode.RSHI: Rsh,
+    Opcode.RSHU: Rsh,
+    Opcode.ADDF: Add,
+    Opcode.SUBF: Sub,
+    Opcode.DIVF: Div,
+    Opcode.MULF: Mul
 }
 
 unary_op_map = {
-    Opcode.NEGI: NegI,
-    Opcode.NEGF: NegF,
+    Opcode.NEGI: Neg,
+    Opcode.NEGF: Neg,
     Opcode.BCOM: BCom    
 }
 
@@ -452,68 +338,90 @@ with open(argv[1], "rb") as f:
     op_stack = []
     ast_nodes = []
     call_args = []
+    basic_block_leaders = set()
+    inst_ast_map = {}
+    inst_idx = 0
+    def add_node(node, i):
+        global inst_idx # TODO idk
+        inst_ast_map[inst_idx] = len(ast_nodes)
+        inst_idx = i + 1
+        ast_nodes.append(node)
     for i in range(instruction_count):
         match code.read(1)[0]:
             case Opcode.UNDEF | Opcode.IGNORE | Opcode.BREAK:
                 pass
             case Opcode.ENTER:
                 assert len(op_stack) == 0, (hex(i), op_stack)
-                if ast_nodes:
-                    print("\n".join(str(n) for n in ast_nodes))
+                if ast_nodes: print(ast_nodes)
                 ast_nodes = []
                 stack_size = _read_int_32(code)
             case Opcode.LEAVE:
-                ast_nodes.append(Return(op_stack.pop()))
+                add_node(Return(op_stack.pop()), i)
                 stack_size = _read_int_32(code) # TODO: assert mb
             case Opcode.CALL:
                 op_stack.append(FunCall(op_stack.pop(), call_args))
                 call_args = []
             case Opcode.PUSH:
-                op_stack.append(None) # TODO: hmmmm
+                op_stack.append(None)
             case Opcode.POP:
-                ast_nodes.append(op_stack.pop())
+                add_node(op_stack.pop(), i)
                 assert len(op_stack) == 0, (hex(i), op_stack) # TODO: is this right?
             case Opcode.CONST:
                 op_stack.append(Constant(_read_int_32(code)))
             case Opcode.LOCAL:
-                offset = _read_int_32(code)
-                op_stack.append(Ref(StackVar(offset))) # TODO: what about args?
+                op_stack.append(Ref(StackVar(_read_int_32(code))))
             case Opcode.JUMP:
-                ast_nodes.append(Goto(op_stack.pop()))
-                 # TODO: make basic block or store info needed to make one
+                target = op_stack.pop()
+                if isinstance(target, Constant):
+                    basic_block_leaders.add(target.value)
+                else:
+                    match ast_nodes:
+                        case [*_, Lt(lhs=_, rhs=Constant(value=min_bound)), Gt(lhs=_, rhs=Constant(value=max_bound))]:
+                            num_entries = max_bound - min_bound + 1
+                            match target:
+                                case Deref(target=Add(lhs=_, rhs=Constant(value=base))):
+                                    base += min_bound * 4
+                                    for addr in range(base, base + num_entries * 4, 4):
+                                        basic_block_leaders.add(int.from_bytes(data[addr:addr+4], 'little'))
+                        case _:
+                            raise Exception(f"Unrecognized switch-case @ {i:#x}")
+                basic_block_leaders.add(i + 1)
+                add_node(Goto(target), i)
             case op if op in comparison_map:
-                target = _read_int_32(code)
+                basic_block_leaders.add(_read_int_32(code))
+                basic_block_leaders.add(i + 1)
                 rhs, lhs = op_stack.pop(), op_stack.pop()
-                ast_nodes.append(comparison_map[op](lhs, rhs))
-                 # TODO: make basic block or store info needed to make one
+                add_node(comparison_map[op](lhs, rhs), i)
             case op if op in bin_op_map:
                 rhs, lhs = op_stack.pop(), op_stack.pop()
                 op_stack.append(bin_op_map[op](lhs, rhs))
             case (Opcode.LOAD1 | Opcode.LOAD2 | Opcode.LOAD4) as op:
-                size = int(2**(op - Opcode.LOAD1)) # TODO: unsketch
+                size = int(2**(op - Opcode.LOAD1))
                 op_stack.append(Deref(size, op_stack.pop()))
             case (Opcode.STORE1 | Opcode.STORE2 | Opcode.STORE4) as op:
                 rhs, lhs = op_stack.pop(), op_stack.pop()
-                size = int(2**(op - Opcode.STORE1)) # TODO: unsketch
-                ast_nodes.append(Assign(Deref(size, lhs), rhs))
+                size = int(2**(op - Opcode.STORE1))
+                add_node(Assign(Deref(size, lhs), rhs), i)
             case Opcode.ARG:
                 arg = op_stack.pop()
-                todo = code.read(1)[0] # TODO: use this...
+                todo = code.read(1)[0] # TODO: maybe use this one day if needed...
                 call_args.append(arg)
             case Opcode.BLOCK_COPY:
                 size = _read_int_32(code)
                 rhs, lhs = op_stack.pop(), op_stack.pop()
-                ast_nodes.append(BlockCopy(size, lhs, rhs))
+                add_node(Assign(Deref(size, lhs), Deref(size, rhs)), i)
             case Opcode.SEX8:
-                pass
+                # TODO: don't need to add casts if input is already the right type
+                # (or have a pass to remove useless casts later)
+                op_stack.append(Cast(Cast(op_stack.pop(), 'char'), 'int'))
             case Opcode.SEX16:
-                pass
+                op_stack.append(Cast(Cast(op_stack.pop(), 'short'), 'int'))
             case op if op in unary_op_map:
                 op_stack.append(unary_op_map[op](op_stack.pop()))
             case Opcode.CVIF:
-                pass
+                op_stack.append(Cast(op_stack.pop(), 'float'))
             case Opcode.CVFI:
-                pass
+                op_stack.append(Cast(op_stack.pop(), 'int'))
             case op:
                 raise Exception(f"Invalid opcode: {op}") # TODO: Find a better class to throw?
-    print("\n".join(str(n) for n in ast_nodes))
+    if ast_nodes: print(ast_nodes)
